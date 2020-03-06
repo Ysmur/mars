b281645273c132d6a67d9dcc10a39bff7487162c
small_teather <gym625.ysmur@yandexlyceum.ru>
2020-03-06 17:43:01 +0300

    v 6.03 - lesson sql2_task4-6 (добавлены форма, шаблон, обработчик для добавления работы). Доделать добавление работы в бд

--- loginform.py
+++ loginform.py
@@ -1,10 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField, DateField
 from wtforms.validators import DataRequired
+from datetime import datetime
 
-
+now = datetime.now()
 class LoginForm(FlaskForm):
     email = StringField('Почта', validators=[DataRequired()])
     password = PasswordField('Пароль', validators=[DataRequired()])
     remember_me = BooleanField('Запомнить меня')
     submit = SubmitField('Войти')
+
+class JobsForm(FlaskForm):
+    team_leader = IntegerField('Руководитель', validators=[DataRequired()])
+    job = StringField('Название работы', validators=[DataRequired()])
+    work_size = IntegerField('Длительность работы', validators=[DataRequired()])
+    collaborators = StringField('Участники', validators=[DataRequired()])
+    start_date = DateField('Дата начала', default=now)
+    end_date = DateField('Дата окончания', default=now)
+    is_finished = BooleanField('Статус', default=False)
+    submit = SubmitField('Создать')
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, request, make_response, session
-from flask_login import LoginManager, login_user
-from loginform import LoginForm
+from flask_login import LoginManager, login_user, logout_user, login_required
+from loginform import LoginForm, JobsForm
 from data import db_session
 from data.users import User
 from data.jobs import Jobs
@@ -79,6 +79,22 @@ def login():
                                message="Неправильный логин или пароль",
                                form=form)
     return render_template('login.html', title='Авторизация', form=form)
+
+@app.route('/addjob', methods=['GET', 'POST'])
+def addjob():
+    form = JobsForm()
+    if form.validate_on_submit():
+        if session:
+
+            return redirect("/")
+        return redirect('/logout')
+    return render_template('addjob.html', title='44', form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
 
 if __name__ == '__main__':
     main()
